888b50d45bad5a01242f643b73f907b931ce596b
Allenz23 <allenz23@nycstudents.net>
2025-05-30 12:22:02 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[] args)
+    {
+      //Scanner scanner = new Scanner(System.in);
+      //System.out.print("Enter your name: ");
+      //String choice = scanner.nextLine();
+      //if (choice.equals("allen")) System.out.println("hi");
+      //else System.out.println("ball");
+      //scanner.close();
+
+
+        //list of spanish questions
+
+        //list of SAT questions
+        //all questions were taken from the official SAT Suite Question Bank
+        
+
+    }
+}
