69b4f745bcc1ecba35986c7866e8f363f1a8802f
Allenz23 <allenz23@nycstudents.net>
2025-05-30 12:22:49 -0400

    Create Spanish.java

---
+++ Spanish.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Spanish extends Questions
+{
+    private ArrayList<String> spanishQuestions = new ArrayList<String>();
+
+    public Spanish(String question, String explanation, String correctAnswer, String false1, String false2, String false3)
+    {
+        super(question, explanation, correctAnswer, false1, false2, false3);
+    }
+
+    public void addQuestion(String s)
+    {
+        
+    }
+}
