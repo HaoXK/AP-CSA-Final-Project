0a02d489c96899a2a06118df91139e9cd81725a4
Allenz23 <allenz23@nycstudents.net>
2025-05-30 12:22:35 -0400

    Create SAT.java

---
+++ SAT.java
@@ -0,0 +1,7 @@
+public class SAT extends Questions
+{
+    public SAT(String question, String explanation, String correctAnswer, String false1, String false2, String false3)
+    {
+        super(question, explanation, correctAnswer, false1, false2, false3);
+    }
+}
