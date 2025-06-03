7517ee7e02a3eb3ac4b3e31e35c89b6b07170f9c
HaoXK <haoxuank@nycstudents.net>
2025-06-02 21:04:09 -0400

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -3,13 +3,12 @@ import java.util.Collections;
 
 public class Questions 
 {
-    private String question;
+    private String passage;
     private String correctAnswer;
     private String false1;
     private String false2;
     private String false3;
     private String explanation;
-    private int position;
 
     //arraylist will always be in the order of {question, choice1, choice2, choice3, choice4}
     private ArrayList<String> fullQuestion = new ArrayList<String>();
@@ -19,15 +18,16 @@ public class Questions
     //nothing, temporary constructor
     }
     
-    public Questions(String question, String explanation, String correctAnswer, String false1, String false2, String false3)
+    //constructor to set up all the variables; ill will call the randomizeQuestion() methods to randomize the order of the answer choices automatically.
+    public Questions(String passage, String explanation, String correctAnswer, String false1, String false2, String false3)
     {
-        this.question = question;
+        this.passage = passage;
         this.correctAnswer = correctAnswer;
         this.false1 = false1;
         this.false2 = false2;
         this.false3 = false3;
         this.explanation = explanation;
-        fullQuestion.add(question);
+        fullQuestion.add(passage);
         fullQuestion.add(correctAnswer);
         fullQuestion.add(false1);
         fullQuestion.add(false2);
@@ -36,6 +36,7 @@ public class Questions
     }    
     //java.util.Collections.shuffle() <- found with the help of StackOverflow
 
+    //randomizes order of the answer choices
     public void randomizeQuestion()
     {
         String temp = fullQuestion.remove(0);
@@ -43,24 +44,35 @@ public class Questions
         fullQuestion.add(0, temp);
     }
 
-    public String getQuestion()
+    //prints the questions in the a, b, c, d format.
+    public String printQuestion()
     {
         return 
-        "question: " + question + 
+        getPassage() +
+        "\n" + "question: " + passage + 
         "\n" + "a: " + fullQuestion.get(1) + 
         "\n" + "b: " + fullQuestion.get(2) +
         "\n" + "c: " + fullQuestion.get(3) +
-        "\n" + "d: " + fullQuestion.get(4)
-        ;
+        "\n" + "d: " + fullQuestion.get(4) +
+        "\n" + "\n";
+        
     }
 
+    //returns the correct answer
+    public String getAnswer()
+    {
+        return correctAnswer;
+    }
+
+    //returns the explanation
     public String getExplanation()
     {
-        return explanation;
+        return explanation + "\n" + "\n";
     }
 
-    public static void indent()
+    //returns the passage
+    public String getPassage()
     {
-        System.out.println();
+        return passage;
     }
 }
