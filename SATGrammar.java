559a695b46a4277700da2bd038eb414263e17f75
HaoXK <haoxuank@nycstudents.net>
2025-06-02 21:03:44 -0400

    Update SATGrammar.java

--- SATGrammar.java
+++ SATGrammar.java
@@ -1,8 +1,7 @@
 public class SATGrammar extends Questions
 {
-    private ArrayList<String> SATQuestions = new ArrayList<String>();
-    public SAT(String question, String explanation, String correctAnswer, String false1, String false2, String false3)
+    public SATGrammar(String passage, String explanation, String correctAnswer, String false1, String false2, String false3)
     {
-        super(question, explanation, correctAnswer, false1, false2, false3);
+        super(passage, explanation, correctAnswer, false1, false2, false3);
     }
 }
